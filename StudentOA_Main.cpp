70a3ef5e7225aa362d5b2a8727e77702c20d3f59
lyj00912 <3122742760@qq.com>
2024-12-10 19:05:48 +0800

    implemented the Display / Search / Append / Exit features.

--- StudentOA_Main.cpp
+++ StudentOA_Main.cpp
@@ -1,6 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <string.h>
 #include <process.h>
+
+/*
+** Global Variables
+*/
 
 struct Student{
 	int id;
@@ -12,6 +17,10 @@ struct Student{
 #define STUDENT_MAX_LEN 1000
 Student students[STUDENT_MAX_LEN];
 int length = 0;
+
+/*
+** Save & Load Functions
+*/
 
 void Save(Student* students, int length){
 	// Initialize
@@ -35,11 +44,11 @@ int Load(Student* buffer){
 		perror("Error Opening File");
 		system("pause");
 	}
-	
+
 	// Pointer Operations
 	int currLength = 0;
 	Student* ptr = buffer;
-	
+
 	// Read from File
 	while (true){
 		fread(ptr, sizeof(Student), 1, fp);
@@ -49,16 +58,139 @@ int Load(Student* buffer){
 		currLength++;
 		ptr++;
 	}
-	
+
 	// Finalize
 	fclose(fp);
-	
+
 	return currLength;
 }
 
-int main(){
-	length = Load(students);
+/*
+** Search Algorithm
+*/
+
+bool isArbitaryStrEqual(char* str1, char* str2, int length){
+	for (int i = 0; i < length; i++){
+		if (str1[i] != str2[i]){
+			return false;
+		}
+	}
+	return true;
+}
+
+bool vagueFind(char* str, char* subStr){
+	int sLen = strlen(str);      // For instance 5
+	int ssLen = strlen(subStr);  // For instance 3
 	
-	int x = 0;
+	for (int starter = 0; starter < sLen - ssLen + 1; starter++){  // 0 1 2
+		char* ptrStarter = str + starter;
+		if (isArbitaryStrEqual(ptrStarter, subStr, ssLen) == true){
+			return true;
+		}
+	}
+	
+	return false;
+}
+
+void Display(){
+	for (int i = 0; i < length; i++){
+		printf(
+			"%d\t%s\t%s\t%d\n",
+			students[i].id,
+			students[i].name,
+			students[i].pwd,
+			students[i].year
+		);
+	}
+	getchar();  // Flush Stdin
+	printf("--------------------------------\n");
+}
+
+void Search(){
+	printf("Please Enter Student's Name, Vague Find Supported: ");
+	char buffer[1000];
+	scanf("%s", buffer);
+	getchar();  // Flush Stdin
+	
+	for (int i = 0; i < length; i++){
+		if (vagueFind(students[i].name, buffer)){
+			printf(
+				"%d\t%s\t%s\t%d\n",
+				students[i].id,
+				students[i].name,
+				students[i].pwd,
+				students[i].year
+			);
+		}
+	}
+	
+	printf("--------------------------------\n");
+}
+
+void Append(){
+	printf("Please Enter the Student's Information that You Wanna Append.\n");
+	printf("Format: id name password year\n");
+	scanf(
+		"%d %s %s %d",
+		&students[length].id,
+		students[length].name,
+		students[length].pwd,
+		&students[length].year
+	);
+	
+	printf(
+		"New Student Appended:\n%d\t%s\t%s\t%d\n",
+		students[length].id,
+		students[length].name,
+		students[length].pwd,
+		students[length].year
+	);
+	length++;   // Step in
+	
+	Save(students, length);
+	printf("Data Saved to the Database.\n");
+	
+	getchar();  // Flush Stdin
+	printf("--------------------------------\n");
+}
+
+int main(){
+	printf("---------- Student OA ----------\n");
+	printf("Enter Your Option, d for Display, s for Search, a for Append, x for Exit.\n");
+	length = Load(students);
+
+	char option;
+	bool wannaExit = false;
+	
+	while (true){
+		printf("Option: ");
+		option = getchar();
+
+		switch (option) {
+		case 'd':
+			Display();
+			break;
+		case 's':
+			Search();
+			break;
+		case 'a':
+			Append();
+			break;
+		case 'x':
+			printf("Exiting...\n");
+			wannaExit = true;
+			break;
+		default:
+			printf("Option NOT FOUND!\n");
+			printf("--------------------------------\n");
+			getchar();  // Flush Stdin
+		}
+		
+		if (wannaExit){
+			break;
+		}
+	}
+
+	return 0;
 }
 
