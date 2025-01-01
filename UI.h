824627dae3d6460177575bb484251d37e02af292
lyj00912 <3122742760@qq.com>
2025-01-01 16:02:56 +0800

    modified and added the missing function declarations in the header file.

--- UI.h
+++ UI.h
@@ -25,7 +25,19 @@ struct Div {
 
 void UI_Init();
 void UI_GotoXY(int x, int y);
-void UI_DrawRect(int x0, int y0, int width, int height, bool drawX, bool drawY);
+void UI_DrawRect(
+	int x0, int y0, int width, int height, bool drawX, bool drawY
+);
+void UI_DrawRect_Animated(
+	int x0, int y0, int width, int height, bool drawX, bool drawY, int frameCount
+);
+void UI_PrintfWordWrap(
+	int x, int y, int maxWidth, int maxHeight, const char* str, bool centered
+);
+void UI_PrintfWordWrap_Animated(
+	int x, int y, int maxWidth, int maxHeight, const char* str_raw, bool centered,
+	int frameCount
+);
 void UI_DrawDiv(Div& div);
 void UI_DrawDiv_Animated(Div& div, int frameCount);
 
